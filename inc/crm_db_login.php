3a997d824e4c4eef6941ddcc5997dc33dfc57126
hirad <m.j.ahmadi88@gmail.com>
2017-10-16 02:03:37 +0330

    m

--- inc/crm_db_login.php
+++ inc/crm_db_login.php
@@ -12,25 +12,27 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 
     while($row = $result->fetch_assoc()) {
-       /* echo "code: " . $row["code"]. " - phone: " . $row["phone"]. "-address: " . $row["address"]. "<br>";
-
-       echo '{ "message": "true" }';*/
+      $suc="true";
     }
 } else {
-    echo '{ "message": "false" }';
+    $suc="false";
 }
-$sql = "SELECT code_product, material, price_co, price_custom, company, des FROM crm";
-$result = $conn->query($sql);
+if ($suc) {
+    $sql = "SELECT code_product, material, price_co, price_custom, company, des FROM crm";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        /*echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-        $age=array("code_product"=>$row["code_product"],"material"=>$row["material"],"price_co"=>$row["price_co"],"company"=>);*/
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            /*echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+            $age=array("code_product"=>$row["code_product"],"material"=>$row["material"],"price_co"=>$row["price_co"],"company"=>);*/
 
-        echo json_encode($row);
+            echo json_encode($row);
+        }
+    } else {
+        echo '{ "message": "false" }';
     }
-} else {
-    echo '{ "message": "oh no" }';
+}else{
+    echo '{ "message": "false" }';
 }
 $conn->close();
