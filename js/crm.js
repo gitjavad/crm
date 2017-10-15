d7f2d6227dc71b95ff9e8ceb35db84efe9d1f2b6
hirad <m.j.ahmadi88@gmail.com>
2017-10-15 20:25:49 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
     $('#Price_list_tb').DataTable( {
         "pagingType": "full_numbers"
     } );
-    $('#price_list_tb').on('load',function (e) {
-        e.preventDefault();
-        $(this).ajaxForm(function () {
-            
-        })
 
-    })
 
 } );
