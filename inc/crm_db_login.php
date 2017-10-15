86cfbdb146499ef57d1145010d1f6d4a279eb8cd
hirad <m.j.ahmadi88@gmail.com>
2017-10-15 21:23:51 +0330

    m

--- inc/crm_db_login.php
+++ inc/crm_db_login.php
@@ -7,9 +7,7 @@ $conn = new mysqli($servername, $username, $pass, $dbname);
 $username=$_POST['code'];
 $password=$_POST['pass'];
 
-
 $sql = "SELECT code FROM crm_login WHERE code='$username' and pass='$password'";
-echo $sql;
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 
@@ -20,30 +18,3 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 $conn->close();
-?>
-/*$servername = "localhost";
-$username = "hirad_admin15023";
-$password = "9133647736!@#";
-$dbname = "hirad-co_com_site";
-$login_user=$_POST('username');
-$login_pass=$_POST('password');
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT user, pass, code, cat, price_co, price_custom, com FROM crm";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-
-while($row = $result->fetch_assoc()) {
-echo "id: " . $row["user"]. " - pass: " . $row["pass"]. "<br>";
-}
-} else {
-echo "0 results";
-}
-$conn->close();
-*/
