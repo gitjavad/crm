5a5cdac6179e111f94f0e1bb40eb46fedefb53b3
hirad <m.j.ahmadi88@gmail.com>
2017-10-15 21:40:50 +0330

    m

--- inc/crm_db_login.php
+++ inc/crm_db_login.php
@@ -6,13 +6,13 @@ $mysql_database = "hirad-co_com_site";
 $conn = new mysqli($hostname, $user, $pass, $mysql_database);
 $username=$_POST['code'];
 $password=$_POST['pass'];
-
 $sql = "SELECT code FROM crm_login WHERE code='$username' AND pass='$password'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
 
     while($row = $result->fetch_assoc()) {
         echo "code: " . $row["code"]. " - phone: " . $row["phone"]. "-address: " . $row["address"]. "<br>";
+        echo '{ "message": "true" }';
     }
 } else {
     echo "0 results";
--- js/crm.js
+++ js/crm.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
     $('#Price_list_tb').DataTable( {
         "pagingType": "full_numbers"
     } );
+    $('#jsonForm').ajaxForm({
 
+        dataType:  'json',
+        success:   processJson
+    });
+    function processJson(data) {
 
+        alert(data.message);
+    }
 } );
