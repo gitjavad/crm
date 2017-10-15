b2916ed2211527de0a4f8dba151c0ea91a299969
hirad <m.j.ahmadi88@gmail.com>
2017-10-16 00:54:22 +0330

    m

--- inc/crm_db_login.php
+++ inc/crm_db_login.php
@@ -4,6 +4,7 @@ $user = "hirad_admin15023";
 $pass= "9133647736!@#";
 $mysql_database = "hirad-co_com_site";
 $conn = new mysqli($hostname, $user, $pass, $mysql_database);
+mysqli_set_charset($conn,'utf8');
 $username=$_POST['code'];
 $password=$_POST['pass'];
 $sql = "SELECT code FROM crm_login WHERE code='$username' AND pass='$password'";
--- js/crm.js
+++ js/crm.js
@@ -1,30 +1,37 @@
 
 $(document).ready(function() {
-    $('#Price_list_tb').DataTable( {
+    $('#Price_list_tb').DataTable({
         "pagingType": "full_numbers"
-    } );
+    });
     $('form').ajaxForm({
 
-        dataType:  'json',
-        success:   processJson
+        dataType: 'json',
+        success: processJson
     });
+
     function processJson(data) {
-var frm_login = document.getElementById('frm_login');
-var main_page = document.getElementById('main_page');
+        var frm_login = document.getElementById('frm_login');
+        var main_page = document.getElementById('main_page');
 
 
-            frm_login.className = frm_login.className.replace(/\b slideInLeft\b/,'');
-            frm_login.className += ' slideOutRight';
-            main_page.style.cssText = 'display:block';
-            document.getElementById('body_list').append("<tr>"+
-            "<td>+ data.code_product+</td>"+
-            "<td>+ data.material+</td>"+
-           "<td>+ data.price_co+</td>"+
-         " <td>+ data.company+</td>"+
-     "<td>+ data.des+</td>"+
-         "</tr>");
-            }
+        frm_login.className = frm_login.className.replace(/\b slideInLeft\b/, '');
+        frm_login.className += ' slideOutRight';
+        main_page.style.cssText = 'display:block';
+        var table = document.getElementById('body_list');
+        var row = table.insertRow(0);
+        var col1 = row.insertCell(0);
+        var col2 = row.insertCell(1);
+        var col3 = row.insertCell(2);
+        var col4 = row.insertCell(3);
+        var col5 = row.insertCell(4);
 
-        }
+        col1.innerHTML = data.code_product;
+        col2.innerHTML = data.material;
+        col3.innerHTML = data.price_co;
+        col4.innerHTML = data.company;
+        col5.innerHTML = data.des;
 
- );
+
+    }
+
+});
