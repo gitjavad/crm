db04c2b01f60dc582e0acbcc02071fdf05bc0bbb
hirad <m.j.ahmadi88@gmail.com>
2017-10-15 23:32:33 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     function processJson(data) {
 var frm_login = document.getElementById('frm_login');
 var main_page = document.getElementById('main_page');
-console.log(data.message["code_product"]);
+console.log(data.message.length);
         /*if(data.message){
 
             frm_login.className = frm_login.className.replace(/\b slideInLeft\b/,'');
