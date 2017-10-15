fdc07ff6be21bae2aba46e964b467f12cbe833ad
hirad <m.j.ahmadi88@gmail.com>
2017-10-15 20:45:20 +0330

    m

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,8 @@
 <?php
 $hostname="localhost";
 $user = "hirad_admin15023";
-$password = "9133647736!@#";
+$pass= "9133647736!@#";
 $mysql_database = "hirad-co_com_site";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $pass, $dbname);
 
 ?>
