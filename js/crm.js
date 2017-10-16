65ea3892fc454a175aac62e1460200896b410001
hirad <m.j.ahmadi88@gmail.com>
2017-10-16 15:05:25 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -12,14 +12,13 @@ $(document).ready(function() {
         for (i=0;i<data.length;i++){
             console.log(data[i].code_product);
         }*/
-if (data[0].message){
+if (data[0].message="true"){
     var frm_login = document.getElementById('frm_login');
     var main_page = document.getElementById('main_page');
     frm_login.className = frm_login.className.replace(/\b slideInLeft\b/, '');
     frm_login.className += ' slideOutRight';
     main_page.style.cssText = 'display:block';
     var table = document.getElementById('body_list');
-
     var i;
     for( i=1;i < data.length;i++){
         var row = table.insertRow(0);
@@ -36,7 +35,7 @@ if (data[0].message){
 
     }}
     else{
-        alert("oh no");
+        console.log("ohno mother fuck get out")
     }
 
 
