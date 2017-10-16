5e26afad3e6e4f7a899b0e6e7504bb0a5dd9d99f
hirad <m.j.ahmadi88@gmail.com>
2017-10-16 19:42:02 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -1,52 +1,55 @@
 
 $(document).ready(function() {
     $('#modal1').modal();
-    $('form').ajaxSubmit({
+    $(".sub_log").click(function () {
+        $('form').ajaxSubmit({
 
-        dataType: 'json',
-        success: processJson
-    });
+            dataType: 'json',
+            success: processJson
+        });
 
-    function processJson(data) {
-        /*var i;
-        for (i=0;i<data.length;i++){
-            console.log(data[i].code_product);
-        }*/
-if (data[0].message=="true"){
-    document.getElementById('main_user_login').style.cssText='display:none';
-    document.getElementById('ok_login').style.cssText='display:block';
-    var frm_login = document.getElementById('frm_login');
-    var main_page = document.getElementById('main_page');
-    frm_login.className = frm_login.className.replace(/\b slideInLeft\b/, '');
-    frm_login.className += ' slideOutRight';
-    main_page.style.cssText = 'display:block';
-    var table = document.getElementById('body_list');
-    var i;
-    for( i=1;i < data.length;i++){
-        var row = table.insertRow(0);
-        var col1 = row.insertCell(0);
-        var col2 = row.insertCell(1);
-        var col3 = row.insertCell(2);
-        var col4 = row.insertCell(3);
-        var col5 = row.insertCell(4);
-        col1.innerHTML = data[i].code_product;
-        col2.innerHTML = data[i].material;
-        col3.innerHTML = data[i].price_co;
-        col4.innerHTML = data[i].company;
-        col5.innerHTML = data[i].des;
+        function processJson(data) {
+            /*var i;
+            for (i=0;i<data.length;i++){
+                console.log(data[i].code_product);
+            }*/
+            if (data[0].message=="true"){
+                document.getElementById('main_user_login').style.cssText='display:none';
+                document.getElementById('ok_login').style.cssText='display:block';
+                var frm_login = document.getElementById('frm_login');
+                var main_page = document.getElementById('main_page');
+                frm_login.className = frm_login.className.replace(/\b slideInLeft\b/, '');
+                frm_login.className += ' slideOutRight';
+                main_page.style.cssText = 'display:block';
+                var table = document.getElementById('body_list');
+                var i;
+                for( i=1;i < data.length;i++){
+                    var row = table.insertRow(0);
+                    var col1 = row.insertCell(0);
+                    var col2 = row.insertCell(1);
+                    var col3 = row.insertCell(2);
+                    var col4 = row.insertCell(3);
+                    var col5 = row.insertCell(4);
+                    col1.innerHTML = data[i].code_product;
+                    col2.innerHTML = data[i].material;
+                    col3.innerHTML = data[i].price_co;
+                    col4.innerHTML = data[i].company;
+                    col5.innerHTML = data[i].des;
 
-    }}
-    else{
-        console.log("ohno mother fuck get out")
-    document.getElementById('main_user_login').style.cssText='display:none';
-    document.getElementById('no_login').style.cssText='display:block';
-    }
-
-
-
+                }}
+            else{
+                console.log("ohno mother fuck get out")
+                document.getElementById('main_user_login').style.cssText='display:none';
+                document.getElementById('no_login').style.cssText='display:block';
+            }
 
 
-    }
+
+
+
+        }
+    })
+
 
 });
 
