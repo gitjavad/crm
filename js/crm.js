2a406eff4e2aecadfb19f7fca6301ce094380506
hirad <m.j.ahmadi88@gmail.com>
2017-10-16 15:01:56 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -12,27 +12,33 @@ $(document).ready(function() {
         for (i=0;i<data.length;i++){
             console.log(data[i].code_product);
         }*/
-console.log(data[0].message);
-        var frm_login = document.getElementById('frm_login');
-        var main_page = document.getElementById('main_page');
-        frm_login.className = frm_login.className.replace(/\b slideInLeft\b/, '');
-        frm_login.className += ' slideOutRight';
-        main_page.style.cssText = 'display:block';
-        var table = document.getElementById('body_list');
+if (data[0].message){
+    var frm_login = document.getElementById('frm_login');
+    var main_page = document.getElementById('main_page');
+    frm_login.className = frm_login.className.replace(/\b slideInLeft\b/, '');
+    frm_login.className += ' slideOutRight';
+    main_page.style.cssText = 'display:block';
+    var table = document.getElementById('body_list');
 
-        var i;
-        for( i=0;i < data.length;i++){
-            var row = table.insertRow(0);
-            var col1 = row.insertCell(0);
-            var col2 = row.insertCell(1);
-            var col3 = row.insertCell(2);
-            var col4 = row.insertCell(3);
-            var col5 = row.insertCell(4);
-            col1.innerHTML = data[i].code_product;
-            col2.innerHTML = data[i].material;
-            col3.innerHTML = data[i].price_co;
-            col4.innerHTML = data[i].company;
-            col5.innerHTML = data[i].des;
+    var i;
+    for( i=1;i < data.length;i++){
+        var row = table.insertRow(0);
+        var col1 = row.insertCell(0);
+        var col2 = row.insertCell(1);
+        var col3 = row.insertCell(2);
+        var col4 = row.insertCell(3);
+        var col5 = row.insertCell(4);
+        col1.innerHTML = data[i].code_product;
+        col2.innerHTML = data[i].material;
+        col3.innerHTML = data[i].price_co;
+        col4.innerHTML = data[i].company;
+        col5.innerHTML = data[i].des;
+
+    }}
+    else{
+        alert("oh no");
+    }
+
 
 
 
