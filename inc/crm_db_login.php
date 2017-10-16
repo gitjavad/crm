139de521a047c3b264c8bf214a1813b323ac2453
hirad <m.j.ahmadi88@gmail.com>
2017-10-16 14:08:28 +0330

    m

--- inc/crm_db_login.php
+++ inc/crm_db_login.php
@@ -6,8 +6,9 @@ $mysql_database = "hirad-co_com_site";
 $conn = new mysqli($hostname, $user, $pass, $mysql_database);
 mysqli_set_charset($conn,'utf8');
 $username=$_POST['code'];
+
+$password=$_POST['pass'];
 echo $username;
-$password="123";/*$_POST['pass'];*/
 $sql = "SELECT code FROM crm_login WHERE code='$username' AND pass='$password'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
