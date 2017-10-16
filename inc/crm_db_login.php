fcb0a0535996b0e9d956d3f9aec8bc635bce7a03
hirad <m.j.ahmadi88@gmail.com>
2017-10-16 14:34:04 +0330

    m

--- inc/crm_db_login.php
+++ inc/crm_db_login.php
@@ -7,27 +7,29 @@ $pass= "9133647736!@#";
 $mysql_database = "hirad-co_com_site";
 $conn = new mysqli($hostname, $user, $pass, $mysql_database);
 mysqli_set_charset($conn,'utf8');
-
+$request_db = array();
 $sql = "SELECT code FROM crm_login WHERE code='$username' AND pass='$password'";
 $result = $conn->query($sql);
+$ok='{ "message": "true" }';
+$notok='{ "message": "false" }';
 if ($result->num_rows > 0) {
 
     while($row = $result->fetch_assoc()) {
 
         $suc = true;
-        echo '{ "message": "true" }';
+        array_push($request_db,$ok);
 
     }
 } else {
     $suc=false;
-
+    array_push($request_db,$notok);
 
 }
 
 if ($suc) {
     $sql = "SELECT code_product, material, price_co, price_custom, company, des FROM crm";
     $result = $conn->query($sql);
-    $request_db = array();
+
 
     if ($result->num_rows > 0) {
 
@@ -42,7 +44,6 @@ if ($suc) {
 
 }
 $conn->close();
-/*if(count($request_db)>0){
+if(count($request_db)>0){
     printf(json_encode($request_db));
 }
-*/
