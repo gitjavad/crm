e6ca41d49dc5acccc96427287091c3efd18e0c75
hirad <m.j.ahmadi88@gmail.com>
2017-10-18 01:55:28 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     $('#input_serch').keydown(function(){
         $('#input_serch').ajaxSubmit({
             dataType:'json',
-            url:'/inc/s_list.php',
+            url:'/crm/inc/s_list.php',
             type:'post',
             success: search
             });
