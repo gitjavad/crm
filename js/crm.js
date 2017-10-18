0d996eef57614ee7209585c5f6a5d40d0b57de4b
hirad <m.j.ahmadi88@gmail.com>
2017-10-19 01:41:27 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -8,6 +8,10 @@ var total
 now_page=1
     pge_con=10
     var first_pg=0;
+$('#body_list td').click(function () {
+    var tb=document.getElementById('body_list');
+    console.log(tb.rowIndex);
+})
     $('#right_sp').click(function () {
        next_page();
 
