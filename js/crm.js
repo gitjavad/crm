bc0d21ceed8f4bc167b408cd85d4b5d8067c9620
hirad <m.j.ahmadi88@gmail.com>
2017-10-18 11:53:04 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $('body').persiaNumber();
     $('#input_num').characterCounter();
     $('#modal1').modal();
-    $('#input_serch').keydown(function(){
+    $('#input_serch').onkeyup(function(){
         $('#s_form').ajaxSubmit({
             dataType:'json',
             success: search
