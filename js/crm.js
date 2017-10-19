79b5aa6b44e71961eafdcad0d0bb43084e0b6f94
hirad <m.j.ahmadi88@gmail.com>
2017-10-19 11:59:38 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -152,7 +152,7 @@ function set_table_row() {
         col4.innerHTML = db[d].price_co;
         col5.innerHTML = db[d].material;
         col6.innerHTML = db[d].des;
-col1.innerHTML='<form class="frm_edit"><input type="text"></input></form>'
+col1.innerHTML+='<form class="frm_edit"><input type="text"></input></form>'
         row.setAttribute('onclick','my_db()')
 
     }
