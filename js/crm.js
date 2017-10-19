404d88a684a6e4f2b858ea36b078acd74f1cefea
hirad <m.j.ahmadi88@gmail.com>
2017-10-19 21:16:42 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -137,7 +137,7 @@ function set_table_row() {
         tb_p_num = db.length
     }
         var d;
-    for (d=first_pg-1;d>pge_con;d--){
+    for (d=first_pg;d>pge_con;d--){
 
         var row = table.insertRow(0);
          col1 = row.insertCell(0);
