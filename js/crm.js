c52425f6e17ee049312016cd50a97599a93b9821
hirad <m.j.ahmadi88@gmail.com>
2017-10-19 09:52:10 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -8,7 +8,9 @@ var total
 now_page=1
     pge_con=10
     var first_pg=0;
-
+    $( "#body_list td" ).on( "click", function() {
+        console.log( $( this ).text() );
+    });
     $('#right_sp').click(function () {
        next_page();
 
