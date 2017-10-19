23c2f6f82aad3a0aaf268905c420b35e492458ea
hirad <m.j.ahmadi88@gmail.com>
2017-10-19 10:48:43 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function() {
+$(document).ready(function($) {
 var data_list;
 var pge_con
 var now_page;
@@ -152,13 +152,13 @@ function set_table_row() {
         col4.innerHTML = db[d].price_co;
         col5.innerHTML = db[d].material;
         col6.innerHTML = db[d].des;
-        col1.setAttribute('onclick','td_run()')
+        col1.setAttribute('onclick','')
 
     }
 }
-    function td_run() {
-        console.log("salam");
-    }
+   $('#body_list td').onclick(function () {
+       console.log("hi")
+   })
 
 
 });
