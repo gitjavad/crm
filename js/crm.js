23ef686db62fa245b0a96761ae56ef3a686d6e9b
hirad <m.j.ahmadi88@gmail.com>
2017-10-19 10:56:37 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -152,13 +152,13 @@ function set_table_row() {
         col4.innerHTML = db[d].price_co;
         col5.innerHTML = db[d].material;
         col6.innerHTML = db[d].des;
-        col1.setAttribute('onclick','')
+        col1.setAttribute('onclick','td_run')
 
     }
 }
-   $('#body_list td').onclick(function () {
-       console.log("hi")
-   })
+  function td_run() {
+      console.log("s");
+  }
 
 
 });
