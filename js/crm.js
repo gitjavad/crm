aa1f473bfeb15e0a225b43da73622c1e2f9008de
hirad <m.j.ahmadi88@gmail.com>
2017-10-20 03:59:42 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -200,7 +200,7 @@ console.log('ok')
 
                     } else {
                         event.target.className+=' animated fadeOut'
-                       console.log(event.target.nodeName)
+                       console.log(event.target.parentNode)
 
                     }
 
