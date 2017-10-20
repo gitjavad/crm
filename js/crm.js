cda3ff6890a90192f848804f41ec6b8242d8c8e9
hirad <m.j.ahmadi88@gmail.com>
2017-10-20 03:50:01 +0330

    m

--- js/crm.js
+++ js/crm.js
@@ -200,6 +200,7 @@ console.log('ok')
 
                     } else {
                         event.target.className+=' animated fadeOut'
+                       console.log( $('#frm_ed').className)
                     }
 
             });
