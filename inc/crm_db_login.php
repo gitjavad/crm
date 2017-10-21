2c43541029737dd76b9da7e38c6ca1dc2ef8f87c
hirad <m.j.ahmadi88@gmail.com>
2017-10-21 12:41:29 +0330

    m

--- inc/crm_db_login.php
+++ inc/crm_db_login.php
@@ -8,7 +8,7 @@ $mysql_database = "hirad-co_com_site";
 $conn = new mysqli($hostname, $user, $pass, $mysql_database);
 mysqli_set_charset($conn,'utf8');
 $request_db = array();
-$ok=array("message" => "true");
+$ok=array("message" => "true","log"=>"ds");
 $notok=array("message" => "false");
 $sql = "SELECT type FROM crm_login WHERE code='".$username."'";
 
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
 if ($result->num_rows>0){
 while($row = $result->fetch_assoc()) {
     if ($row['type']=="admin"){
-        $user_chek=("{type:admin}");
+        $user_chek=array("type"=>"admin");
         array_push($ok,$user_chek) ;
         array_push($request_db,$ok);
     }else{
